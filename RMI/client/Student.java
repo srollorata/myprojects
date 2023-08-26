16b0c2e57700d1662514640d8a2111dfb190ea75
wyn327 <selwynrollorata14@gmail.com>
2023-08-26 14:37:12 +0800

    CRUD JAVA RMI (Netbeans-re)

---
+++ RMI/client/Student.java
@@ -0,0 +1,67 @@
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ *
+ * @author ADMIN
+ */
+
+import java.io.Serializable;
+
+public class Student implements Serializable {
+    private String name;
+    private int age;
+    private String address;
+    private String contact;
+
+    public Student(String name, int age, String address, String contact) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+        this.contact = contact;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", address='" + address + '\'' +
+                ", contact='" + contact + '\'' +
+                '}';
+    }
+}
